984798652b50b12931a3e8f0eb8f0d71bf4f9f34
jamesjjj48 <jamesjjj48@74156adb-be44-0410-a8bd-599819d3de0a>
2008-03-02 22:39:30 +0000

    more initialization added to project2 final

--- test/project2_final.c
+++ test/project2_final.c
@@ -65,13 +65,18 @@ typedef struct
 
 typedef struct
 {
+	int status;
+	int ID;
+}Spot;
+
+typedef struct
+{
 	int lineID;
 	int preferLineCount;
 	int regLineCount;
-	int preferLineQueue;
-	int regLineQueue;
 	int money;
-	
+	Spot preferLine[NUM_OF_CUSTOMER];
+	Spot regLine[NUM_OF_CUSTOMER];
 }cLine;
 
 
@@ -80,10 +85,10 @@ cTable pictureTable = {1, 0, 0, 0, 0};
 cTable passportTable = {2, 0, 0, 0, 0};
 cTable cashierTable = {3, 0, 0, 0, 0};
 
-cLine applicationLine = {0, 0, 0, 0, 0, 0};
-cLine pictureLine = {1, 0, 0, 0, 0, 0};
-cLine passportLine = {2, 0, 0, 0, 0, 0};
-cLine cashierLine = {3, 0, 0, 0, 0, 0};
+cLine applicationLine = {0, 0, 0, 0};
+cLine pictureLine = {1, 0, 0, 0};
+cLine passportLine = {2, 0, 0, 0};
+cLine cashierLine = {3, 0, 0, 0};
 
 Customer customerArray[NUM_OF_CUSTOMER];
 Clerk applicationClerkArray[NUM_OF_CLERK];
@@ -92,14 +97,35 @@ Clerk passportClerkArray[NUM_OF_CLERK];
 Clerk cashierClerkArray[NUM_OF_CLERK];
 
 
+void customerRun()
+{
+	int myOwnID = -1;
+	Acquire(customerIDLock);
+	myOwnID = customerIDCount;
+	customerIDCount++;
+	Release(customerIDLock);
+	
+	
+	
+	Exit(0);
+}
 
-
-void customerRun(Customer myCustomer)
+void applicationClerkRun()
 {
 	Exit(0);
 }
 
-void clerkRun(Clerk myClerk)
+void pictureClerkRun()
+{
+	Exit(0);
+}
+
+void passportClerkRun()
+{
+	Exit(0);
+}
+
+void cashierClerkRun()
 {
 	Exit(0);
 }
@@ -134,47 +160,73 @@ void Initialize()
 	pictureTableCond = CreateCondition();
 	passportTableCond = CreateCondition();
 	cashierTableCond = CreateCondition();
-
 	
-	/* Create Manager, Customer and Clerk threads */
+	/*Initialize values for the four lines, clean them up*/
+	for(i=0; i<NUM_OF_CUSTOMER; i++)
+	{
+		applicationLine.regLine[i].ID = -1;
+		applicationLine.regLine[i].status = 0;
+		applicationLine.preferLine[i].ID = -1;
+		applicationLine.preferLine[i].status = 0;
+	}
+	for(i=0; i<NUM_OF_CUSTOMER; i++)
+	{
+		pictureLine.regLine[i].ID = -1;
+		pictureLine.regLine[i].status = 0;
+		pictureLine.preferLine[i].ID = -1;
+		pictureLine.preferLine[i].status = 0;
+	}for(i=0; i<NUM_OF_CUSTOMER; i++)
+	{
+		passportLine.regLine[i].ID = -1;
+		passportLine.regLine[i].status = 0;
+		passportLine.preferLine[i].ID = -1;
+		passportLine.preferLine[i].status = 0;
+	}for(i=0; i<NUM_OF_CUSTOMER; i++)
+	{
+		cashierLine.regLine[i].ID = -1;
+		cashierLine.regLine[i].status = 0;
+		cashierLine.preferLine[i].ID = -1;
+		cashierLine.preferLine[i].status = 0;
+	}
 	
 	/* Creating Clerks for application table*/
 	for(i=0; i<NUM_OF_CLERK; i++)
 	{
-		applicationClerkArray[i].clerkID = i;
 		applicationClerkArray[i].responsibleTable = 0;
-		applicationClerkArray[i].status = 1;
+		fork(applicationClerkRun);
 	}
 	
 	/* Creating Clerks for picture table*/
 	for(i=0; i<NUM_OF_CLERK; i++)
 	{
-		pictureClerkArray[i].clerkID = i;
-		pictureClerkArray[i].responsibleTable = 0;
-		pictureClerkArray[i].status = 1;
+		pictureClerkArray[i].responsibleTable = 1;
+		fork(pictureClerkRun);
 	}
 	
 	/* Creating Clerks for passport table*/
 	for(i=0; i<NUM_OF_CLERK; i++)
 	{
-		passportClerkArray[i].clerkID = i;
-		passportClerkArray[i].responsibleTable = 0;
-		passportClerkArray[i].status = 1;
+		passportClerkArray[i].responsibleTable = 2;
+		fork(passportClerkRun);
 	}
 	
 	/* Creating Clerks for cashier table*/
 	for(i=0; i<NUM_OF_CLERK; i++)
 	{
-		cashierClerkArray[i].clerkID = i;
-		cashierClerkArray[i].responsibleTable = 0;
-		cashierClerkArray[i].status = 1;
+		cashierClerkArray[i].responsibleTable = 3;
+		fork(pictureClerkRun);
 	}
 	
 	/*Creating Customers*/
 	for(i=0; i<NUM_OF_CUSTOMER; i++)
 	{
-		
-	}	
+		customerArray[i].money = 1600;
+		customerArray[i].applicationDone = 0;
+		customerArray[i].pictureDone = 0;
+		customerArray[i].passportDone = 0;
+		customerArray[i].cashierDone = 0;
+		fork(customerRun);
+	}
 	
 	/*Create Manager*/
 	
