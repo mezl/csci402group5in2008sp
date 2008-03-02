82100648a6d52dd8cd23ab4b4ba34c8383ea748f
mezlxx <mezlxx@74156adb-be44-0410-a8bd-599819d3de0a>
2008-03-02 00:09:27 +0000

    compile test

--- test/lockcond.c
+++ test/lockcond.c
@@ -19,90 +19,99 @@ synchonization described above.
 
 
 void func1(){
-  print("func 1 start");
+  println("[func1] start");
   Acquire(lock1);
-  println("func1 signals on cond1 with lock1\n");
+  println("[func1] signals on cond1 with lock1");
   Signal(lock1,cond1);
 
-  Acquire(lock2);
+ /* Acquire(lock2);*/
   Release(lock1);
- 
-  println("func1 waits on cond3 with lock2\n");
+/* 
+  println("func1 waits on cond3 with lock2");
   Wait(lock2,cond3);
   Release(lock2);
 
 
   Acquire(lock1);
-  println("func1 signals on cond1 with lock1\n");
+  println("func1 signals on cond1 with lock1");
   Signal(lock1,cond1);
   Release(lock1);
-
-  println("func1 exits\n");
+*/
+  println("[func1]func1 exits");
+  
   Exit(0);
 }
 
 void func2(){
 
+  println("[func2] start");
   Acquire(lock1);
-  println("func2 signals on cond2 with lock1\n");
+  println("[func2] signals on cond2 with lock1");
   Signal(lock1,cond2);
 
-  Acquire(lock2);
+/*  Acquire(lock2);*/
   Release(lock1);
-
+/*
   println("func2 waits on cond3 with lock2\n");
   Wait(lock2,cond3);
   Release(lock2);
   
-
-  println("func2 exits\n");
+*/
+  println("[func2] exits");
   Exit(0);
 }
 
 int main()
 {
 
-  print("Main!  start");
-  Write("Start test\n",11,ConsoleOutput);
+  println("[main]Main! start");
+  println("[main]Start test");
   lock1 = CreateLock();
   lock2 = CreateLock();
   cond1 = CreateCondition();
   cond2 = CreateCondition();
-  cond3 = CreateCondition();
+/*  cond3 = CreateCondition();*/
 
-  Write("Create All Lock & Cond\n",23,ConsoleOutput);
+  println("[main]Create All Lock & Cond");
   Acquire(lock1);
-  Write("func1 forked by main\n",21,ConsoleOutput);
+  println("[main]func1 forked by main");
   Fork(func1);
-  Write("main waits on cond1 with lock1\n",31,ConsoleOutput);
+  println("[main]main waits on cond1 with lock1");
   Wait(lock1,cond1);
+  println("[main]Main releaseing lock1");
   Release(lock1);
 
-
+  println("[main]Main acquiring lock1");
   Acquire(lock1);
-  Write("func2 forked by main\n",21,ConsoleOutput);
+  println("[main]func2 will fork by main");
   Fork(func2);
-  Write("main waits on cond2 with lock1\n",31,ConsoleOutput);
+  println("[main]func2 forked by main,now wait from func2");
   Wait(lock1,cond2);
+  println("[main]main wake up on cond2 with lock1");
+  println("[main]Main releaseing lock1");
   Release(lock1);
+  println("[main]Main released lock1");
 
-
+/*
   Acquire(lock2);
-  Write("main broadcasts on cond3 with lock2\n",36,ConsoleOutput);
+  println("main broadcasts on cond3 with lock2\n");
   Broadcast(lock2,cond3);
 
 
   Acquire(lock1);
   Release(lock2);
 
-
-  Write("main waits on cond1 with lock1\n",31,ConsoleOutput);
+*/
+  println("[main]main waits on cond1 with lock1\n");
+/*  
   Wait(lock1,cond1);
   Release(lock1);
+*/
 
 
 
-  Write("main exits\n",12,ConsoleOutput);
+  println("[main]main exits\n");
+
 
   DestroyLock(lock1);
   DestroyLock(lock2);
