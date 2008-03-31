53c313501508a11b60c165d1332dc244fe68acce
mezlxx <mezlxx@74156adb-be44-0410-a8bd-599819d3de0a>
2008-03-31 10:02:22 +0000

    add new Test file test_exit

---
+++ test/test_exit.c
@@ -0,0 +1,19 @@
+/* 
+test for exit system calls 
+main 
+*/
+
+
+#include "syscall.h"
+#include "string.h"
+
+int main()
+{
+
+//  printf("[main]Main! start\n");
+//  printf("[main]main exits\n");
+
+  Exit(0);
+}
+
+
