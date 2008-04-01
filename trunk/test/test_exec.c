ef0362988f46bd966df1d1ce1bc8c7f7002a825e
mezlxx <mezlxx@74156adb-be44-0410-a8bd-599819d3de0a>
2008-04-01 02:34:02 +0000

    finish test

--- trunk/test/test_exec.c
+++ trunk/test/test_exec.c
@@ -12,10 +12,14 @@ int main()
 
   println("[Exec main]Main! start");
 
-  println("Exec the 1st fork test file");
-	Exec("test_fork");
+  println("Exec the 1st exec test file");
+	Exec("test_exit");
 
   println("Exec the 2nd fork test file");
+	Exec("test_exit");
+  println("[Exec main]main exits");
+  
+  println("Exec the 3rd exit test file");
 	Exec("test_fork");
   println("[Exec main]main exits");
 
