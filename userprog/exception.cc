d45faa44a757db7f3948d35e7a13a865bc640c0f
mezlxx <mezlxx@74156adb-be44-0410-a8bd-599819d3de0a>
2008-04-02 00:36:14 +0000

    remove error message in exit thread

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -544,20 +544,18 @@ void Exit_Syscall(int status)
 	
 	if(check == -1)
 	{
-		printf("Can't exit current thread\n");
+	//	printf("Can't exit current thread\n");
 	}
 	if(check == 1 || check == 2)
 		currentThread->space->~AddrSpace();
 	if(check == 2){//if it's last thread and last process
 		interrupt->Halt();
    }
-	if(check == 0)
+	if(check == 0 || status == 0)
 		currentThread->Finish();
-	if(status == 0)
-		currentThread->Finish();
-	else
-		printf("Cannot exit current thread\n");
-
+	else{
+//		printf("Cannot exit current thread\n");
+   }
 }
 #endif
 
